a71b5ab06b7f6b41a632804f8d68913b3d2474b4
RazorneckCodes <48930043+RazorneckCodes@users.noreply.github.com>
2021-06-05 11:13:24 +0200

    read_data update on serial error

--- src/rpi/read_data_serial_raw.py
+++ src/rpi/read_data_serial_raw.py
@@ -4,10 +4,18 @@ import time
 from influxdb import InfluxDBClient
 
 def read_data(command): 
-    req = command + "\n"
-    ser.write(req.encode())
-    line = ser.readline().decode('utf-8').rstrip()
-    return float(line)
+    try:
+        req = command + "\n"
+        ser.write(req.encode())
+        line = ser.readline(10).decode('utf-8').rstrip()
+        return float(line)
+    except:
+        return None
+        #reset serial port and arduino
+        #ser.close()
+        #ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+        #ser.flushInput()
+        #ser.close()
 
 def con_influx():
     user = ''
@@ -17,7 +25,6 @@ def con_influx():
     port = 8086
     client = InfluxDBClient(host, port, user, password, dbname, ssl=True, verify_ssl=True)
     return client
-
 
 def send_db(co2, temp, hum, res):
     client = con_influx()
@@ -41,16 +48,17 @@ def send_db(co2, temp, hum, res):
             }
         }
     ]
+    print(json)
     client.write_points(json, time_precision='s')
 
 if __name__ == '__main__':
-        ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) #search interface name at py
-        ser.flush()
-        time.sleep(2) #time to open up the serial connection
-
-        co2 = read_data("co2")
-        humidity = read_data("hum")
-        temperature = read_data("temp")
-        gas_resistance = read_data("res")
+        ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+        ser.flushInput()
+        time.sleep(3) #time to open up serial monitor
+        co2 = read_data("co2", ser)
+        humidity = read_data("hum", ser)
+        temperature = read_data("temp", ser)
+        gas_resistance = read_data("res", ser)
         send_db(co2, temperature, humidity, gas_resistance)
+        ser.close()
 
